76cb1b3eb431d01e8ba39d6826c64a00e737cac1
zhanghan <izhanghan@outlook.com>
2016-12-09 00:26:16 +0800

    a

--- index.php
+++ index.php
@@ -7,25 +7,6 @@
 	<title>查询</title>
 </head>
 <body>
-<script>
-	function search(){
-			alert(111)
-			let xhr = new XMLHttpRequest();
-			alert(222)
-			let bikeNum = document.querySelector('#bikeNum').value;
-			alert(333)
-			xhr.onreadystatechange = function()
-		  {
-		  if (xhr.readyState==4 && xhr.status==200)
-		    {
-			    document.getElementById("password").value=xhr.responseText;
-		    }
-		  }
-			xhr.open("get","./search.php?bikeNum="+ bikeNum,true);
-			xhr.send();
-			alert(444)
-		}
-</script>
 <div class="container">
 	<div class="tips">
 	  请输入车牌号，稍等会密码就会出现！
@@ -37,15 +18,28 @@
 			<input type="text" name="password" placeholder="密码" disabled="true"  id="password">
 		</div>
 		<div style="margin-top: 20px">
-			<div class="btn-round" style="margin-top: 20px;background-color: #2ac87d" onclick="search()">查询</a>
+			<div class="btn-round" style="margin-top: 20px;background-color: #2ac87d" onclick="fn_seach()">查询</a>
 		</div>
 	<a href="lu.php" class="btn-round" style="margin-top: 20px">去录入</a>
 </div>
 <script>
-function fn(){
-		alert(666)
-	}
-		
+	function fn_seach(){
+			alert(111)
+			// let xhr = new XMLHttpRequest();
+			// alert(222)
+			// let bikeNum = document.querySelector('#bikeNum').value;
+			// alert(333)
+			// xhr.onreadystatechange = function()
+		  // {
+		  // if (xhr.readyState==4 && xhr.status==200)
+		  //   {
+			//     document.getElementById("password").value=xhr.responseText;
+		  //   }
+		  // }
+			// xhr.open("get","./search.php?bikeNum="+ bikeNum,true);
+			// xhr.send();
+			// alert(444)
+		}
 	</script>
 </body>
 </html>
